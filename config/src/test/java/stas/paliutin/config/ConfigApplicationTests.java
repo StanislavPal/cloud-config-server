87cb5a77da549d8ea356408dd7b643573ea96d36
Stanislav <paliutin.stanislav@gmail.com>
2021-02-09 03:04:41 +0400

    commit init

---
+++ config/src/test/java/stas/paliutin/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package stas.paliutin.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
